02902e1183e77c0904e0b9b3777593926b5eeeac
Lucca Bonatto <devbonatto@gmail.com>
2025-05-20 10:33:46 -0300

    :sparkles: feat: adding notes editor

--- app/src/main/java/com/fmu/notesapp/NotesTakerActivity.java
+++ app/src/main/java/com/fmu/notesapp/NotesTakerActivity.java
@@ -22,6 +22,7 @@ public class NotesTakerActivity extends AppCompatActivity implements Serializabl
     EditText editText_title, editText_notes;
     ImageView imageView_save;
     Notes notes;
+    boolean isOldNote = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,16 @@ public class NotesTakerActivity extends AppCompatActivity implements Serializabl
         imageView_save = findViewById(R.id.imageView_save);
         editText_title = findViewById(R.id.editText_title);
         editText_notes = findViewById(R.id.editText_notes);
+
+        notes = new Notes();
+        try {
+            notes = (Notes) getIntent().getSerializableExtra("old_note");
+            editText_title.setText(notes.getTitle());
+            editText_notes.setText(notes.getNotes());
+            isOldNote = true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         imageView_save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +58,9 @@ public class NotesTakerActivity extends AppCompatActivity implements Serializabl
                 Date date = new Date();
                 String dataFormatada = formatter.format(date);
 
-                notes = new Notes();
+                if (!isOldNote){
+                    notes = new Notes();
+                }
 
                 notes.setTitle(title);
                 notes.setNotes(description);
